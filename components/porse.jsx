a147a858bb8076dd0b3f1bfc0112e94e36c76596
ronil <ronil@7span.com>
2025-11-18 19:43:48 +0530

    modify prose styling

--- components/porse.jsx
+++ components/porse.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function Porse({value}) {
+export default function Porse({ value }) {
   return (
-      <div className="prose prose-h2:mt-1" dangerouslySetInnerHTML={{ __html: value }} />
+    // <div className="prose prose-h2:my-0 prose-blockquote:my-2 prose-p:mb-6 " dangerouslySetInnerHTML={{ __html: value }} />
+    <div
+      className="prose max-w-none 
+             [&_p]:mb-4
+             [&_p]:mt-0 
+             [&_h2]:my-0 
+             [&_blockquote]:my-4
+             [&_ul]:my-4 
+             [&_ol]:my-4"
+      dangerouslySetInnerHTML={{ __html: value }}
+    />
   )
 }
